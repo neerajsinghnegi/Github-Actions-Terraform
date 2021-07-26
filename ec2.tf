d890c66280fe6aab12efa9bba7f6e801b0279cd5
Neeraj Singh Negi <60294234+neerajsinghnegi@users.noreply.github.com>
2021-07-26 23:36:33 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,20 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+terraform {
   resource "aws_instance" "app_server" {
   ami           = "ami-0ad704c126371a549"
   instance_type = "t2.micro"
-
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
 
   backend "remote" {         
        organization = "neerajsinghnegi"
